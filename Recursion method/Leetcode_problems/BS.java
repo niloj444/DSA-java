f60fc2d0bae063e2114f8ced6dd8b522d5fd8478
niloj444 <mniloj07@gmail.com>
2026-07-03 21:46:40 +0530

    done

---
+++ Recursion method/Leetcode_problems/BS.java
@@ -0,0 +1,25 @@
+class BS{
+
+    public int search(int[] nums, int target) {
+        return helper(nums, target, 0, nums.length - 1);
+    }
+
+    private int helper(int[] nums, int target, int start, int end) {
+
+        if (start > end) {
+            return -1;
+        }
+
+        int mid = start + (end - start) / 2;
+
+        if (nums[mid] == target) {
+            return mid;
+        }
+
+        if (target < nums[mid]) {
+            return helper(nums, target, start, mid - 1);
+        }
+
+        return helper(nums, target, mid + 1, end);
+    }
+}
