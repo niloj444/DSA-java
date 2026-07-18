b903644edc437388e4cf8130117791f3548537f8
niloj444 <mniloj07@gmail.com>
2026-07-18 23:33:24 +0530

    push

---
+++ Recursion method/RotatedBS.java
@@ -0,0 +1,36 @@
+public class RotatedBS {
+    public static void main(String[] args) {
+        int[] arr={5,6,7,8,9,1,2,3};
+        System.out.println(search(arr, 8, 0, arr.length-1));
+    }
+    static int search (int [] arr, int target, int s, int e){
+        if(s>e){
+            return -1;
+        }
+        int mid= s+(e-s)/2;
+
+        if(arr[mid]==target){
+            return mid;
+        }
+
+        //check for left half
+        if(arr[s]<= arr[mid]){
+            if(target>= arr[s] && target <=arr[mid]){
+                return search(arr, target, s, mid-1);
+            }
+            else{
+                return search(arr, target, mid+1, e);
+            }
+        }
+        if(target>= arr[mid] && target <=arr[e]){
+                return search(arr, target, mid+1, e);
+            }
+            else{
+                return search(arr, target, s, mid-1);
+            }
+        
+
+        
+
+    }
+}
